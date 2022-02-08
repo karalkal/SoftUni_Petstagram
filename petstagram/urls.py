91f06af2fadafdd67e75b1ed4b764def2768bb09
karalkal <karalkal@yahoo.co.uk>
2022-02-08 21:29:10 +0000

    Removed redundant directory

--- petstagram/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-import petstagram
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('petstagram.main_app.urls'))
-]
