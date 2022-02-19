1976af775774c06ce169c57e7e1be6d2da11cf48
kk <karalkal@yahoo.co.uk>
2022-02-19 14:29:55 +0000

    Remove duplicated directory

--- SoftUni_Petstagram/SoftUni_Petstagram/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-import SoftUni_Petstagram
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('SoftUni_Petstagram.main_app.urls'))
-]
